84c1f34442dc21b8c7a4484b855cfef8676700af
Kouki <k.arima.cingroup2012@gmail.com>
2023-11-08 15:59:30 +0900

    if文提出1回目

---
+++ php/if/lesson02.php
@@ -0,0 +1,11 @@
+﻿<?php
+$score = 39; //いくつかのケースで動作確認を行ってください。
+if($score >= 80){
+    echo $score,'点は「優」です。';
+}elseif($score >= 60){
+    echo $score,'点は「良」です。';
+}elseif($score >= 40){
+    echo $score,'点は「可」です。';
+}else{
+    echo $score,'点は「不可」です。';
+}
